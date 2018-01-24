f1ef3e454f4cb27c882f3f685d5ebc9dde19063a
benmacha <contact@benmacha.tn>
2018-01-24 11:36:34 +0100

    first commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Lucky Stream panel.
+ *
+ * @category   IPTV
+ *
+ * @author     Ali Ben Macha       <contact@benmacha.tn>
+ * @author     Azri Bilel          <azri.bilel@gmail.com>
+ *
+ * @see        http://dali.benmacha.tn/
+ */
+
+namespace TemplateBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('TemplateBundle:Default:index.html.twig');
+    }
+}
---
+++ TemplateBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Lucky Stream panel.
+ *
+ * @category   IPTV
+ *
+ * @author     Ali Ben Macha       <contact@benmacha.tn>
+ * @author     Azri Bilel          <azri.bilel@gmail.com>
+ *
+ * @see        http://dali.benmacha.tn/
+ */
+
+namespace TemplateBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TemplateBundle extends Bundle
+{
+}
